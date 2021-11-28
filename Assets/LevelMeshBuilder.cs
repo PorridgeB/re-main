d4699563fd5d1827d665e9a10103dcb431f793cc
Clive Patrick <clive.m.patrick@gmail.com>
2021-11-28 14:21:36 +1100

    Added collision mesh to level

--- Assets/LevelMeshBuilder.cs
+++ Assets/LevelMeshBuilder.cs
@@ -7,12 +7,28 @@ public class LevelMeshBuilder : MonoBehaviour
 {
     public Tilemap WallsTilemap;
     public Tilemap FloorsTilemap;
-
-    // TODO: Generate collision mesh
-    // TODO: Generate navmesh
-
-    // Start is called before the first frame update
+    
     void Start()
+    {
+        var meshFilter = GetComponent<MeshFilter>();
+        meshFilter.sharedMesh = GenerateMesh();
+
+        var meshCollider = GetComponent<MeshCollider>();
+        meshCollider.sharedMesh = GenerateCollisionMesh();
+    }
+
+    void AddQuad(int firstVertexIndex, List<int> triangles)
+    {
+        triangles.Add(firstVertexIndex);
+        triangles.Add(firstVertexIndex + 2);
+        triangles.Add(firstVertexIndex + 1);
+
+        triangles.Add(firstVertexIndex);
+        triangles.Add(firstVertexIndex + 3);
+        triangles.Add(firstVertexIndex + 2);
+    }
+
+    Mesh GenerateMesh()
     {
         List<Vector3> vertices = new List<Vector3>();
         List<Vector2> uvs = new List<Vector2>();
@@ -22,7 +38,7 @@ public class LevelMeshBuilder : MonoBehaviour
         foreach (var cell in FloorsTilemap.cellBounds.allPositionsWithin)
         {
             var tile = FloorsTilemap.GetTile(cell);
-            
+
             if (tile != null)
             {
                 var floorSprite = FloorsTilemap.GetSprite(cell);
@@ -121,22 +137,75 @@ public class LevelMeshBuilder : MonoBehaviour
         mesh.RecalculateBounds();
         mesh.Optimize();
 
-        var meshFilter = GetComponent<MeshFilter>();
-        meshFilter.sharedMesh = mesh;
+        return mesh;
     }
 
-    void AddQuad(int firstVertexIndex, List<int> triangles)
+    Mesh GenerateCollisionMesh()
     {
-        triangles.Add(firstVertexIndex);
-        triangles.Add(firstVertexIndex + 2);
-        triangles.Add(firstVertexIndex + 1);
+        List<Vector3> vertices = new List<Vector3>();
+        List<Vector2> uvs = new List<Vector2>();
+        List<int> triangles = new List<int>();
 
-        triangles.Add(firstVertexIndex);
-        triangles.Add(firstVertexIndex + 3);
-        triangles.Add(firstVertexIndex + 2);
+        foreach (var cell in FloorsTilemap.cellBounds.allPositionsWithin)
+        {
+            var tile = FloorsTilemap.GetTile(cell);
+
+            if (tile != null)
+            {
+                var firstVertexIndex = vertices.Count;
+
+                vertices.Add(new Vector3(cell.x, 0, cell.y));
+                vertices.Add(new Vector3(cell.x + 1, 0, cell.y));
+                vertices.Add(new Vector3(cell.x + 1, 0, cell.y + 1));
+                vertices.Add(new Vector3(cell.x, 0, cell.y + 1));
+
+                AddQuad(firstVertexIndex, triangles);
+
+                // Generate collision mesh for adjacent walls
+                Vector3[][] wallVertexOffsets = new Vector3[][]
+                {
+                    new Vector3[] { new Vector3(0, 1, 0), new Vector3(1, 1, 0), new Vector3(1, 0, 0), new Vector3(0, 0, 0) },
+                    new Vector3[] { new Vector3(1, 1, 0), new Vector3(1, 1, 1), new Vector3(1, 0, 1), new Vector3(1, 0, 0) },
+                    new Vector3[] { new Vector3(1, 1, 1), new Vector3(0, 1, 1), new Vector3(0, 0, 1), new Vector3(1, 0, 1) },
+                    new Vector3[] { new Vector3(0, 1, 1), new Vector3(0, 1, 0), new Vector3(0, 0, 0), new Vector3(0, 0, 1) }
+                };
+
+                Vector3Int[] wallOffsets = new Vector3Int[] { new Vector3Int(0, -1, 0), new Vector3Int(1, 0, 0), new Vector3Int(0, 1, 0), new Vector3Int(-1, 0, 0) };
+
+                for (int i = 0; i < wallOffsets.Length; i++)
+                {
+                    Vector3Int wallOffset = wallOffsets[i];
+
+                    if (!FloorsTilemap.HasTile(cell + wallOffset))
+                    {
+                        firstVertexIndex = vertices.Count;
+
+                        Vector3 worldPosition = new Vector3(cell.x, 0, cell.y);
+
+                        foreach (Vector3 wallVertexOffset in wallVertexOffsets[i])
+                        {
+                            vertices.Add(worldPosition + wallVertexOffset);
+                        }
+
+                        AddQuad(firstVertexIndex, triangles);
+                    }
+                }
+            }
+        }
+
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices.ToArray();
+        mesh.uv = uvs.ToArray();
+        mesh.triangles = triangles.ToArray();
+
+        mesh.RecalculateNormals();
+        mesh.RecalculateTangents();
+        mesh.RecalculateBounds();
+        mesh.Optimize();
+
+        return mesh;
     }
 
-    // Update is called once per frame
     void Update()
     {
         
