c9137fda03c71f0d470297a6d395ba07cce8fef7
Clive Patrick <clive.m.patrick@gmail.com>
2021-12-03 22:27:40 +1100

    Added lerp to 3D camera

--- Assets/Scripts/CameraController3D.cs
+++ Assets/Scripts/CameraController3D.cs
@@ -5,7 +5,9 @@ using UnityEngine.InputSystem;
 
 public class CameraController3D : MonoBehaviour
 {
+    // What the camera will follow
     public Transform Target;
+    public float Speed = 20f;
 
     private float height;
 
@@ -17,8 +19,10 @@ public class CameraController3D : MonoBehaviour
     void Update()
     {
         Camera.main.ResetWorldToCameraMatrix();
+        
+        var targetPosition = new Vector3(Target.position.x, height, Target.position.z - height);
 
-        transform.position = new Vector3(Target.position.x, height, Target.position.z - height);
+        transform.position = Vector3.Lerp(transform.position, targetPosition, Speed * Time.deltaTime);
 
         Camera.main.worldToCameraMatrix = Matrix4x4.Rotate(Quaternion.AngleAxis(-45, Vector3.right)) * Matrix4x4.Scale(new Vector3(1, Mathf.Sqrt(2), Mathf.Sqrt(2))) * Camera.main.worldToCameraMatrix;
     }
