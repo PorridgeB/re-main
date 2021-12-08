2f0d9723682fac602a2d6188c3f07dfc655abf72
Clive Patrick <clive.m.patrick@gmail.com>
2021-12-08 21:02:35 +1100

    Added better wandering behaviour

--- Assets/Scripts/Tasks/RandomPosition.cs
+++ Assets/Scripts/Tasks/RandomPosition.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using BehaviorDesigner.Runtime;
 using BehaviorDesigner.Runtime.Tasks;
+using UnityEngine.AI;
 
 public class RandomPosition : Action
 {
     public SharedVector3 Position;
+    public float Radius = 10f;
 
     public override void OnStart()
     {
-        Position = new Vector3(10, 0, 10);
+        var offset = Random.insideUnitCircle * Radius;
+        var randomPosition = transform.position + new Vector3(offset.x, 0, offset.y);
+
+        NavMeshHit hit;
+        NavMesh.SamplePosition(randomPosition, out hit, Radius, 1);
+
+        Position.Value = hit.position;
     }
 
     public override TaskStatus OnUpdate()
