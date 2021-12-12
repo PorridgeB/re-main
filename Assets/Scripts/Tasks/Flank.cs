c5f466050b6f78423ad4d9419fae9db9633f8f0a
Clive Patrick <clive.m.patrick@gmail.com>
2021-12-12 17:13:04 +1100

    Added flanking behaviour to Gnats

--- Assets/Scripts/Tasks/Flank.cs
+++ Assets/Scripts/Tasks/Flank.cs
@@ -3,7 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 using BehaviorDesigner.Runtime;
 using BehaviorDesigner.Runtime.Tasks;
+using UnityEngine.AI;
 
 public class Flank : Action
 {
+    public SharedFloat Speed;
+    // 1 for a perfect orbit around target, 0 for moving directly towards target
+    public SharedFloat Orbit;
+    public SharedTransform Target;
+
+    private NavMeshAgent agent;
+    // Orbit in a clockwise rotation
+    private bool clockwise = false;
+
+    public override void OnStart()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        clockwise = Random.value > 0.5f;
+    }
+
+    public override TaskStatus OnUpdate()
+    {
+        // TODO: Change to Target
+        var targetDirection = (PlayerController.instance.transform.position - transform.position).normalized;
+        var targetDirection2D = new Vector2(targetDirection.x, targetDirection.z).normalized;
+
+        var orbitDirection2D = Vector2.Perpendicular(targetDirection2D) * (clockwise ? -1 : 1);
+
+        var direction2D = Vector2.Lerp(targetDirection2D, orbitDirection2D, Orbit.Value);
+
+        var velocity2D = direction2D * Speed.Value;
+
+        agent.Move(new Vector3(velocity2D.x, 0, velocity2D.y) * Time.deltaTime);
+
+        return TaskStatus.Running;
+    }
 }
