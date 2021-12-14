cb7140ca158dd234e63809e156109747254881b6
Clive Patrick <clive.m.patrick@gmail.com>
2021-12-14 15:05:59 +1100

    Added low resolution

--- Assets/Scripts/CameraController3D.cs
+++ Assets/Scripts/CameraController3D.cs
@@ -8,12 +8,18 @@ public class CameraController3D : MonoBehaviour
     // What the camera will follow
     public Transform Target;
     public float Speed = 20f;
+    public int PixelsPerUnit = 16;
+    public bool Smooth = false;
+    public bool SnapToNearestPixel = true;
 
     private float height;
 
     void Start()
     {
         height = transform.position.y;
+
+        var camera = GetComponent<Camera>();
+        camera.orthographicSize = (camera.targetTexture.height / 2f) / PixelsPerUnit;
     }
 
     void Update()
@@ -22,7 +28,23 @@ public class CameraController3D : MonoBehaviour
         
         var targetPosition = new Vector3(Target.position.x, height, Target.position.z - height);
 
-        transform.position = Vector3.Lerp(transform.position, targetPosition, Speed * Time.deltaTime);
+        if (Smooth)
+        {
+            transform.position = Vector3.Lerp(transform.position, targetPosition, Speed * Time.deltaTime);
+        }
+        else
+        {
+            transform.position = targetPosition;
+        }
+
+        if (SnapToNearestPixel)
+        {
+            var nearestX = Mathf.Round(transform.position.x * PixelsPerUnit) / PixelsPerUnit;
+            var nearestY = Mathf.Round(transform.position.y * PixelsPerUnit) / PixelsPerUnit;
+            var nearestZ = Mathf.Round(transform.position.z * PixelsPerUnit) / PixelsPerUnit;
+
+            transform.position = new Vector3(nearestX, nearestY, nearestZ);
+        }
 
         Camera.main.worldToCameraMatrix = Matrix4x4.Rotate(Quaternion.AngleAxis(-45, Vector3.right)) * Matrix4x4.Scale(new Vector3(1, Mathf.Sqrt(2), Mathf.Sqrt(2))) * Camera.main.worldToCameraMatrix;
     }
