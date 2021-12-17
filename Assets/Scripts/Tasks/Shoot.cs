15380db44979d799322422b38a307077ad67330c
Clive Patrick <clive.m.patrick@gmail.com>
2021-12-17 17:23:34 +1100

    Removed obsolete tasks

--- Assets/Scripts/Tasks/Shoot.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using BehaviorDesigner.Runtime;
-using BehaviorDesigner.Runtime.Tasks;
-
-public class Shoot : Action
-{
-}
