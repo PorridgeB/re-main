67bb2150e4a0a639835df8b2f2efafbbd3aa51f8
Clive Patrick <clive.m.patrick@gmail.com>
2021-12-22 18:46:49 +1100

    Added early abort to Gnat priming

--- Assets/Scripts/Gnat.cs
+++ Assets/Scripts/Gnat.cs
@@ -1,3 +1,4 @@
+using BehaviorDesigner.Runtime;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,13 @@ public class Gnat : MonoBehaviour
 {
     public GameObject ExplosionPrefab;
     public float ExplosionDamage = 50f;
+
+    public void Prime()
+    {
+        var behaviorTree = GetComponent<BehaviorTree>();
+
+        behaviorTree.SetVariableValue("Primed", true);
+    }
 
     public void Detonate()
     {
