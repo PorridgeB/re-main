1b8cde654353f6122ace63f6924aa9dea9f56832
PorridgeB <49393032+PorridgeB@users.noreply.github.com>
2021-12-29 16:11:24 +1100

    ADD: Pits, which can only be crossed by dashing if the player can make it to the other side.

--- Assets/Scripts/StateMachine/Player/PlayerDash.cs
+++ Assets/Scripts/StateMachine/Player/PlayerDash.cs
@@ -12,6 +12,8 @@ public class PlayerDash : StateMachineBehaviour
     private float dashTimer;
     private float dashSoundRange;
     private Vector2 dashDirection;
+    [SerializeField]
+    private GameObject cube;
 
     private void SetCollisionWithEnemies(bool enabled)
     {
@@ -21,13 +23,43 @@ public class PlayerDash : StateMachineBehaviour
         Physics.IgnoreLayerCollision(playerLayer, enemiesLayer, !enabled);
     }
 
+    private void SetCollisionWithDashable(bool enabled)
+    {
+        var dashableLayer = LayerMask.NameToLayer("Dashable");
+        var playerLayer = LayerMask.NameToLayer("Player");
+
+        Physics.IgnoreLayerCollision(playerLayer, dashableLayer, !enabled);
+    }
+
+    private void CheckEndpointClear()
+    {
+        Vector3 origin = GameObject.Find("Player").transform.position + new Vector3(dashDirection.x, 0, dashDirection.y) * 5.5f;
+        Collider[] cols = Physics.OverlapBox(origin, new Vector3(0.1f, 0.1f, 0.1f));
+        foreach (Collider c in cols)
+        {
+            Debug.Log(c.name);
+            if (c.gameObject.layer == LayerMask.NameToLayer("Dashable"))
+            {
+                Debug.Log("cant dash here");
+                return;
+            }
+        }
+        SetCollisionWithDashable(false);
+    }
+
     public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         // Ignore collision with Enemies
         SetCollisionWithEnemies(false);
+        
+
+
 
         dashTimer = 0;
         dashDirection = new Vector2(animator.GetFloat("VelX"), animator.GetFloat("VelY"));
+
+        CheckEndpointClear();
+
         if (dashDirection == Vector2.zero)
         {
             dashDirection = new Vector2(animator.GetFloat("Horizontal"), animator.GetFloat("Vertical")).normalized;
@@ -44,6 +76,7 @@ public class PlayerDash : StateMachineBehaviour
     {
         // Enable collision with Enemies
         SetCollisionWithEnemies(true);
+        SetCollisionWithDashable(true);
 
         PlayerController.instance.Stop();
     }
