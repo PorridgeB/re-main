6be4bdafb93f704b2aabc2f05a89928e8463c579
Clive Patrick <clive.m.patrick@gmail.com>
2021-12-29 14:20:06 +1100

    Added quick light fade to projectile impact

--- Assets/ProjectileImpact.cs
+++ Assets/ProjectileImpact.cs
@@ -6,22 +6,31 @@ public class ProjectileImpact : MonoBehaviour
 {
     public Color Color;
 
+    private new Light light;
+    private float maxTime = 1f;
+    private float startTime;
+    private float endTime;
+
     void Start()
     {
-        var light = GetComponentInChildren<Light>();
+        light = GetComponentInChildren<Light>();
         light.color = Color;
 
-        var spriteRenderer = GetComponentInChildren<SpriteRenderer>();
-        spriteRenderer.color = Color;
+        //var spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        //spriteRenderer.color = Color;
 
         var particleSystem = GetComponentInChildren<ParticleSystem>();
         var main = particleSystem.main;
         main.startColor = Color;
 
-        Destroy(gameObject, 1f);
+        startTime = Time.time;
+        endTime = startTime + maxTime;
+
+        Destroy(gameObject, maxTime);
     }
 
     void Update()
     {
+        light.intensity = 1 - Mathf.InverseLerp(startTime, endTime, Time.time);
     }
 }
