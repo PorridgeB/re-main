5b00546300a56a7547888323c3c9521cbc366ac5
Clive Patrick <clive.m.patrick@gmail.com>
2022-01-05 23:21:44 +1100

    Update RoomMeshOptions.cs

--- Assets/Scripts/RoomMesh/RoomMeshOptions.cs
+++ Assets/Scripts/RoomMesh/RoomMeshOptions.cs
@@ -6,7 +6,10 @@ using UnityEngine;
 [Serializable]
 public class RoomMeshOptions
 {
+    [Tooltip("Add the hidden back and side faces to the walls")]
     public bool AddHiddenWallFaces = true;
-    public bool AddAmbientOcclusion = true;
+    [Tooltip("Bake fake ambient occlusion into the mesh vertex colors")]
+    public bool BakeAmbientOcclusion = true;
+    [Tooltip("The tint of the ambient occlusion")]
     public Color AmbientOcclusionColor = new Color(0.4f, 0.4f, 0.5f);
 }
