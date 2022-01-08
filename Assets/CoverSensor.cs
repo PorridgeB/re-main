1bc9d8a2aef71a3e0b1e4c0662ffed9fdec095ab
Clive Patrick <clive.m.patrick@gmail.com>
2022-01-08 19:33:50 +1100

    Implemented cover sensor

--- Assets/CoverSensor.cs
+++ Assets/CoverSensor.cs
@@ -4,8 +4,28 @@ using UnityEngine;
 
 public class CoverSensor : Sensor
 {
+    public float MaxDistance = 10;
+
     public override void Sense()
     {
+        var covers = GameObject.FindGameObjectsWithTag("Cover");
 
+        foreach (var cover in covers)
+        {
+            if ((cover.transform.position - transform.position).magnitude > MaxDistance)
+            {
+                continue;
+            }
+
+            var direction = (cover.transform.position - (transform.position + Vector3.up)).normalized;
+
+            if (Physics.Raycast(transform.position + Vector3.up, direction, out RaycastHit hit, MaxDistance, LayerMask.GetMask("Level")))
+            {
+                if ((hit.point - cover.transform.position).magnitude < 1)
+                {
+                    memory.Record(new Observation(cover.transform.position, cover));
+                }
+            }
+        }
     }
 }
