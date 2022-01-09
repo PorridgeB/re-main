afb95473204dc81abebfed65192e2414db7b6d05
Clive Patrick <clive.m.patrick@gmail.com>
2022-01-09 14:56:16 +1100

    Simplified WIthinSight task

--- Assets/Scripts/Tasks/WithinSight.cs
+++ Assets/Scripts/Tasks/WithinSight.cs
@@ -9,7 +9,6 @@ public class WithinSight : Conditional
 {
     public SharedFloat SightDistance;
     public SharedGameObject Target;
-    public SharedLayerMask Mask = new LayerMask { value = LayerMask.GetMask("Player") | LayerMask.GetMask("Level") };
 
     public override TaskStatus OnUpdate()
     {
@@ -19,13 +18,9 @@ public class WithinSight : Conditional
         }
 
         var direction = (Target.Value.transform.position - transform.position).normalized;
-
-        if (Physics.Raycast(transform.position + Vector3.up, direction, out RaycastHit raycastHit, SightDistance.Value, Mask.Value))
+        if (!Physics.Raycast(transform.position + Vector3.up, direction, out _, SightDistance.Value, LayerMask.GetMask("Level")))
         {
-            if (raycastHit.collider.gameObject == Target.Value)
-            {
-                return TaskStatus.Success;
-            }
+            return TaskStatus.Success;
         }
 
         return TaskStatus.Failure;
