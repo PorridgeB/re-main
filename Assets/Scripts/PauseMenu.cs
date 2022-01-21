5f828af17e455a9bc442234cfb87bf20be8c3f19
Clive Patrick <clive.m.patrick@gmail.com>
2022-01-21 12:36:15 +1100

    Changed look of resources

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -10,8 +10,8 @@ public class PauseMenu : MonoBehaviour
 
     private void Start()
     {
-        var image = GetComponent<Image>();
-        LeanTween.value(gameObject, a => image.color = new Color(0, 0, 0, a), 0.4f, 0, 0.5f).setEaseInOutExpo().setLoopPingPong();
+        //var image = GetComponent<Image>();
+        //LeanTween.value(gameObject, a => image.color = new Color(0, 0, 0, a), 0.4f, 0, 0.5f).setEaseInOutExpo().setLoopPingPong();
 
         //var rectTrans = Panel.GetComponent<RectTransform>();
         //LeanTween.moveX(rectTrans, -rectTrans.rect.width, 0.5f).setEaseInOutExpo().setLoopPingPong();
