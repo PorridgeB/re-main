bcb6d269e1e1aeb9cbd5bcdd0b0202d6264f7505
Clive Patrick <clive.m.patrick@gmail.com>
2022-01-21 14:34:32 +1100

    Fixed UI sounds

---
+++ Assets/Scripts/SaveProfile.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class SaveProfile : MonoBehaviour
+{
+    public TextMeshProUGUI Text;
+
+    private void Start()
+    {
+        Text.text = "Empty";
+
+        if (Random.value < 0.5f)
+        {
+            return;
+        }
+
+        var totalTime = "00:00:00";
+        var story = 0;
+        var dataFragments = 0;
+        var scrap = 0;
+
+        Text.text = $"Total Time - {totalTime}\tStory - {story}%\nData Fragments - {dataFragments}\tScrap - {scrap}";
+    }
+}
