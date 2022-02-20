2bb76ad80524afc8f25c7dcd45e8e5ebd1209da5
Clive Patrick <clive.m.patrick@gmail.com>
2022-02-20 23:19:11 +1100

    Added rough start of intro sequence

---
+++ Assets/StoryText.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class StoryText : MonoBehaviour
+{
+    [Range(0, 1)]
+    public float PercentageRevealed = 0;
+    [Range(0.1f, 2)]
+    public float CaretBlinkPeriod = 1;
+    public string Caret = "█";
+    public AudioClip Character;
+
+    [SerializeField]
+    private AudioSource audioSource;
+    private TextMeshProUGUI textMesh;
+    private string text;
+    private float caretTimer;
+    private int previousLength;
+
+    private void Awake()
+    {
+        textMesh = GetComponent<TextMeshProUGUI>();
+        text = textMesh.text;
+        caretTimer = Time.time;
+        previousLength = 0;
+    }
+
+    private void Update()
+    {
+        if (Time.time - caretTimer > CaretBlinkPeriod)
+        {
+            caretTimer = Time.time;
+        }
+
+        var revealedLength = Mathf.RoundToInt(text.Length * PercentageRevealed);
+
+        //var revealedText = text.Substring(0, revealedLength);
+        //textMesh.text = revealedText;
+
+        textMesh.maxVisibleCharacters = revealedLength;
+
+        if (previousLength < revealedLength)
+        {
+            audioSource.PlayOneShot(Character);
+        }
+
+        previousLength = revealedLength;
+
+        //if ((Time.time - caretTimer) / CaretBlinkPeriod > 0.5f)
+        //{
+        //    textMesh.text += Caret;
+        //}
+    }
+}
