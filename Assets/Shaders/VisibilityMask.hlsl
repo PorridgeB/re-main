dc427d051cd36664b195420fe4b778d3b5502013
Clive Patrick <clive.m.patrick@gmail.com>
2022-02-26 21:13:40 +1100

    Added deferred rendering

--- Assets/Shaders/VisibilityMask.hlsl
+++ Assets/Shaders/VisibilityMask.hlsl
@@ -3,11 +3,19 @@
 
 void CalculateVisibilityMask_float(float3 Position, out float3 Color)
 {
-#ifndef SHADERGRAPH_PREVIEW
-    Light light = GetAdditionalLight(0, Position, 1);
-    Color = light.shadowAttenuation;
-#else
     Color = 0;
+#ifndef SHADERGRAPH_PREVIEW
+    uint additionalLightsCount = GetAdditionalLightsCount();
+    for (uint i = 0; i < additionalLightsCount; i++)
+    {
+        Light light = GetAdditionalLight(i, Position, 1);
+        //if (light.layerMask == (1 << 6))
+        if (true)
+        {
+            Color = light.shadowAttenuation;
+            break;
+        }
+    }
 #endif
 }
 
